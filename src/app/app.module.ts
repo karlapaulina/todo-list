b77bf00b30331fd1f41053647cb096a7fbcb23f0
Karla Gonzalez <49318447+karlapaulina@users.noreply.github.com>
2020-04-03 21:22:06 -0400

    feat: create todo and todo-item components and add basic global styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoComponent } from './conponents/todo/todo.component';
+import { TodoItemComponent } from './conponents/todo-item/todo-item.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, TodoComponent, TodoItemComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  //services to include
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
